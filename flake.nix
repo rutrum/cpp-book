9fbc27c27e22069a5e01f962ae2e2a313aa350e2
rutrum <dave@rutrum.net>
2023-08-23 07:34:19 -0400

    Use utils for all systems

--- flake.nix
+++ flake.nix
@@ -1,21 +1,22 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages.${system}.default = pkgs.stdenv.mkDerivation { 
-      pname = "cpp-book";
-      version = "0.0.1";
-      src = ./.;
-      nativeBuildInputs = with pkgs; [ mdbook ];
-      buildPhase = "mdbook build";
-      installPhase = "cp -r book $out";
-    };
-
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [ mdbook ];
-    };
-  };
+  outputs = { self, nixpkgs, utils }: 
+    utils.lib.eachDefaultSystem(system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.default = pkgs.stdenv.mkDerivation { 
+        pname = "cpp-book";
+        version = "0.0.1";
+        src = ./.;
+        nativeBuildInputs = with pkgs; [ mdbook ];
+        buildPhase = "mdbook build";
+        installPhase = "cp -r book $out";
+      };
+  
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ mdbook ];
+      };
+  });
 }
